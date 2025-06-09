193001bb5a0d056831c4db048b3b026b3c7152a3
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2025-06-09 19:11:56 +0530

    Memory Utilization Alert

---
+++ memory_alert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###Start###
+
+THRESHOLD=70
+for i in `cat /admin/hostname`;
+do
+RH_MEM_LOAD=$(ssh $i free -m | grep Mem | awk '{ printf("%.2f\n", $3/$2 * 100) }')
+HOSTNAME=$(ssh $i hostname -I | awk '{print $1}')
+
+if [[ $RH_MEM_LOAD > $THRESHOLD ]];
+then
+        echo "Memory Utilization is Reached $RH_MEM_LOAD % on $HOSTNAME" | mail -s "Memory Utilization is High" laptop@example.com,labuser@example.com
+else
+        echo "$HOSTNAME Memory Load is normal"  >> /admin/mem_load.csv
+fi
+done
+
+###End###
