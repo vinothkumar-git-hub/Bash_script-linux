8770f5587746f8d97d923502eb43b231cefc6dc2
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2026-01-24 20:52:05 +0530

    memory alert mail

--- memory_alert.sh
+++ memory_alert.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-###Start###
+threshold=75
 
-THRESHOLD=70
-for i in `cat /admin/hostname`;
-do
-RH_MEM_LOAD=$(ssh $i free -m | grep Mem | awk '{ printf("%.2f\n", $3/$2 * 100) }')
-HOSTNAME=$(ssh $i hostname -I | awk '{print $1}')
+hostname=$(hostname -I | awk '{print $1}')
+used_mem=$(free -m | grep 'Mem:' | awk '{print $3/$2*100}' | cut -c 1-2)
 
-if [[ $RH_MEM_LOAD > $THRESHOLD ]];
+if [ $used_mem -ge $threshold ];
 then
-        echo "Memory Utilization is Reached $RH_MEM_LOAD % on $HOSTNAME" | mail -s "Memory Utilization is High" testuser1@example.com,testuser2@example.com
-else
-        echo "$HOSTNAME Memory Load is normal"  >> /admin/mem_load.csv
+        echo "Memory is Full on $hostname $(date)" | mail -s "Memory Utilization is High" testuser1@example.com,testuser2@example.com
 fi
-done
-
-###End###
