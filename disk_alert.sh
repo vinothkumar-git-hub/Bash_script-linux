b80e69e82cc4da56074c18e855bd62ae1d7ce5c7
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2026-01-24 20:45:11 +0530

    Create disk_alert.sh
    Disk usage Alert

---
+++ disk_alert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+THRESHOLD=75
+
+####Fetching IP Address####
+hostname=$(hostname -I | awk '{print $1}')
+
+#######Print partition and disk usage######
+
+df -h | grep -vE 'Filesystem|devtmpfs|tmpfs' | awk '{print $1 " "$5}i' | while read output;
+do
+        used_percentage=$(echo $output | awk '{print $2}' | sed 's/%//')
+        partition=$(echo $output | awk '{print $1}')
+
+        if [ $used_percentage -ge $THRESHOLD ];
+        then
+                echo "$partition is Reached $used_percentage % on IP $hostname $(date)"
+        fi
+done
