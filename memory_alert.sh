e9fbbaf9a98449c95ad5f244c620aed0ca3612b1
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2026-01-24 20:46:15 +0530

    Update memory_alert.sh

--- memory_alert.sh
+++ memory_alert.sh
@@ -9,7 +9,7 @@ HOSTNAME=$(ssh $i hostname -I | awk '{print $1}')
 
 if [[ $RH_MEM_LOAD > $THRESHOLD ]];
 then
-        echo "Memory Utilization is Reached $RH_MEM_LOAD % on $HOSTNAME" | mail -s "Memory Utilization is High" laptop@example.com,labuser@example.com
+        echo "Memory Utilization is Reached $RH_MEM_LOAD % on $HOSTNAME" | mail -s "Memory Utilization is High" testuser1@example.com,testuser2@example.com
 else
         echo "$HOSTNAME Memory Load is normal"  >> /admin/mem_load.csv
 fi
