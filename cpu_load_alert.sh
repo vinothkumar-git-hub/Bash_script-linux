c3d3bb0963080a9e3a6152cdb1a506523469ae3b
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2026-01-27 19:22:07 +0530

    CPU Load Alert

--- cpu_load_alert.sh
+++ cpu_load_alert.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
-###Start###
+# Threshold for CPU load
 
-threshold=70
-for i in `cat /admin/hostname`;
-do
-load=$(ssh $i uptime | awk '{ printf("%.2f\n", $8) }')
-HOSTNAME=$(ssh $i hostname -I | awk '{print $1}')
+THRESHOLD=70.00
+ip_addr=$(hostname -I | awk '{print $1}')
 
-if [[ $load > $threshold ]];
-then
-        echo "CPU load is $load Critical on $HOSTNAME" | mail -s "CPU Mail Alert" laptop@example.com,labuser@example.com
-else
-        echo "$HOSTNAME CPU load is normal" >> /admin/cpuload.log
+LOAD=$(uptime | awk -F'load average:' '{print $2}' | cut -d',' -f1 | tr -d ' ')
+
+# Compare load using bc (binary calculator)
+
+if (( $(echo "$LOAD >= $THRESHOLD" | bc -l) )); then
+    echo "CPU Load is Critical ($LOAD) on $ip_addr " | mail -s "CPU Mail Alert" laptop@example.com,labuser@example.com
 fi
-done
-
-###End###
