e8c0109ef800f5c906593ece5117f12ef6eb7cf4
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2026-02-05 16:56:20 +0530

    Create user and provide sudo access

--- usercreation.sh
+++ usercreation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-echo -e "Create login User for Aipscanner: \c"
-read -r user
-egrep "^$user" /etc/passwd >/dev/null
-if [ $? -eq 0 ]; then
-        echo "$user user already exists!!"
 
+read -p "Enter the Username: " username
+
+egrep -q ^$username /etc/passwd > /dev/null 2&>1
+
+if [ $? -eq 0 ];
+then
+        echo "User already exists!!"
 else
-        echo -e "Enter Password: \c"
-        read -s password
-        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-        useradd -m -p "$pass" "$user"
-        usermod -s /bin/bash $user
-        mkdir -p /home/$user
-        chown $user:$user /home/$user
-        echo "$user ALL=(ALL)   NOPASSWD: ALL" >> /etc/sudoers
-        echo "$user user added successfully"
+        echo "Creating $username"
+        read -s -p "Enter the password for $username " password
+
+        ##Create User and Provide Sudo Privilage##
+        useradd -m $username
+        echo "$username:$password" | chpasswd
+        echo "$username ALL=(ALL)   NOPASSWD: ALL" >> /etc/sudoers
+        echo
 fi
